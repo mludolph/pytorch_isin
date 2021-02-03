abbc9950d43aa1d00b9c37ad4c0fd4743975b9b6
Moritz Ludolph <mail@moritzludolph.de>
2021-02-03 20:49:31 +0100

    add tests

--- test/test_isin.py
+++ test/test_isin.py
@@ -1,25 +1,30 @@
 from itertools import product
 
+import numpy as np
 import pytest
 import torch
 from torch import Tensor
 from torch_isin import isin
 
-from .utils import dtypes, devices, tensor
+from .utils import devices, dtypes, tensor, rand_tensor
+
+
+@pytest.mark.parametrize("device,invert", product(devices, [True, False]))
+def test_isin(device, invert):
+    elements_np = np.arange(-5, 5, dtype=np.int32)
+    # elements_np = np.repeat(np.repeat(elements_np[None], 5, 0)[None], 5, 0)
+    test_elements_np = np.arange(-3, 3, dtype=np.int32)
+    isin_np = np.isin(elements_np, test_elements_np, invert=invert).astype(np.uint8)
+
+    elements = tensor(elements_np, dtype=torch.int, device=device)
+    test_elements = tensor(test_elements_np, dtype=torch.int, device=device)
+
+    isin_torch = isin(elements, test_elements, invert=invert)
+    print(isin_torch)
+    print(isin_np)
+    assert (isin_torch.cpu().numpy() == isin_np).all()
 
 
 @pytest.mark.parametrize("dtype,device", product(dtypes, devices))
-def test_isin(dtype, device):
+def test_random_isin(dtype, device):
     pass
-
-
-@pytest.mark.parametrize("device", devices)
-def test_random_isin(device):
-    N = 1024
-    for _ in range(5):
-        pos = torch.randn((2 * N, 3), device=device)
-        batch_1 = torch.zeros(N, dtype=torch.long, device=device)
-        batch_2 = torch.ones(N, dtype=torch.long, device=device)
-        batch = torch.cat([batch_1, batch_2])
-        idx = fps(pos, batch, ratio=0.5)
-        assert idx.min() >= 0 and idx.max() < 2 * N
--- test/utils.py
+++ test/utils.py
@@ -9,3 +9,10 @@ if torch.cuda.is_available():
 
 def tensor(x, dtype, device):
     return None if x is None else torch.tensor(x, dtype=dtype, device=device)
+
+
+def rand_tensor(size, dtype, device, low=-1000, high=1000):
+    if dtype == torch.float or dtype == torch.double:
+        return ((low - high) * torch.rand(size) + high).type(dtype)
+    elif dtype == torch.int or dtype == torch.long:
+        return torch.randint(low, high, size, device=device).type(dtype)
